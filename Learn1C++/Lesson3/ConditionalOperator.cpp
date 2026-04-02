2ba8a4ed75018af93fdb328c74fad982c32e86c4
Sergharm <sergharm1991@gmail.com>
2026-04-02 20:18:48 +0700

    Чётвертая задача

--- Learn1C++/Lesson3/ConditionalOperator.cpp
+++ Learn1C++/Lesson3/ConditionalOperator.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 int main(){
-    int A, B, C;
-    cin >> A >> B >> C;
-    if(A < (B + C) && (B < (C + A) && (C < (B + A)))){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if((a == c) && (((b - 1) == d) || ((b + 1 ) == d) || (b == d))){
         cout << "YES";
     }
-    
+    else if((b == d) && (((a -1) == c) || ((a + 1) == c) || (a == c))){
+        cout << "YES";
+    }
+    else if ((((a + 1) == c) && (b + 1) == d) || (((a - 1) == c) && ((b + 1) == d)) || ((((a + 1) == c) && ((b - 1) == d))) || ((((a - 1) == c) && (b + 1) == d))){
+        cout << "YES";
+    }
     else{
         cout << "NO";
     }
-    return 0;
+    return 0;  
 }
