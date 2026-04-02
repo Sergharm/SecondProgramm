02b70c05db4e4e6a2ff0d4bb1fc6852d32c01ff5
Sergharm <sergharm1991@gmail.com>
2026-04-02 16:41:44 +0700

    Вторая задача

--- Learn1C++/Lesson3/ConditionalOperator.cpp
+++ Learn1C++/Lesson3/ConditionalOperator.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main(){
-    int a, b;
-    cin >> a >> b;
-    if (a > b){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if ((a >= b) && (a >= c)){
         cout << a;
     }
-    else if(a == b){
-        cout << a;
-    }
-    else{
+    if ((b > a) && (b >= c)){
         cout << b;
+    }
+    if ((c > a) && (c > b)){
+        cout << c;
     }
     return 0;
 }
