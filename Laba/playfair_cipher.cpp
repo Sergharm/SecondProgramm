759ba06aaa001ddd46c039255d61de82b5fdaeb3
Sergharm <sergharm1991@gmail.com>
2026-06-04 01:44:50 +0700

    Улучшен Playfair cipher: добавлена таблица подстановки 16x16

--- Laba/playfair_cipher.cpp
+++ Laba/playfair_cipher.cpp
@@ -1,6 +1,38 @@
 #include "crypto_interface.h"
 #include <cstring>
 
+static void build_table(ConstBuffer key, uint8_t table[16][16]) {
+    bool used[256] = {false};
+    uint8_t flat[256];
+    size_t pos = 0;
+    
+    for (size_t i = 0; i < key.size; i++) {
+        if (!used[key.data[i]]) {
+            used[key.data[i]] = true;
+            flat[pos++] = key.data[i];
+        }
+    }
+    
+    for (int i = 0; i < 256; i++) {
+        if (!used[i]) flat[pos++] = i;
+    }
+    
+    for (int r = 0; r < 16; r++)
+        for (int c = 0; c < 16; c++)
+            table[r][c] = flat[r * 16 + c];
+}
+
+static void find_pos(uint8_t table[16][16], uint8_t val, int& row, int& col) {
+    for (int r = 0; r < 16; r++) {
+        for (int c = 0; c < 16; c++) {
+            if (table[r][c] == val) {
+                row = r; col = c;
+                return;
+            }
+        }
+    }
+}
+
 extern "C" {
 
 const AlgorithmInfo* get_algorithm_info() {
@@ -15,16 +47,45 @@ size_t get_output_size(size_t input_size, int operation_type) {
 int encrypt(ConstBuffer key, ConstBuffer input, MutBuffer* output) {
     if (key.size < 16) return 1;
     
+    uint8_t table[16][16];
+    build_table(key, table);
+    
     for (size_t i = 0; i < input.size; i += 2) {
-        output->data[i] = input.data[i] ^ key.data[i % key.size];
-        if (i + 1 < input.size)
-            output->data[i + 1] = input.data[i + 1] ^ key.data[(i + 1) % key.size];
+        int r1, c1, r2, c2;
+        find_pos(table, input.data[i], r1, c1);
+        find_pos(table, (i + 1 < input.size) ? input.data[i+1] : 0, r2, c2);
+        
+        if (r1 == r2) {
+            output->data[i] = table[r1][(c1 + 1) % 16];
+        } else if (c1 == c2) {
+            output->data[i] = table[(r1 + 1) % 16][c1];
+        } else {
+            output->data[i] = table[r1][c2];
+        }
     }
     return 0;
 }
 
 int decrypt(ConstBuffer key, ConstBuffer input, MutBuffer* output) {
-    return encrypt(key, input, output);
+    if (key.size < 16) return 1;
+    
+    uint8_t table[16][16];
+    build_table(key, table);
+    
+    for (size_t i = 0; i < input.size; i += 2) {
+        int r1, c1, r2, c2;
+        find_pos(table, input.data[i], r1, c1);
+        find_pos(table, (i + 1 < input.size) ? input.data[i+1] : 0, r2, c2);
+        
+        if (r1 == r2) {
+            output->data[i] = table[r1][(c1 + 15) % 16];
+        } else if (c1 == c2) {
+            output->data[i] = table[(r1 + 15) % 16][c1];
+        } else {
+            output->data[i] = table[r1][c2];
+        }
+    }
+    return 0;
 }
 
 }
