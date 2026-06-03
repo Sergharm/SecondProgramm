fcfdabb64d8a81522a744ac9ea284b59cd806d5d
Sergharm <sergharm1991@gmail.com>
2026-06-04 01:44:50 +0700

    Добавлены базовые структуры для работы с буферами и информацией об алгоритмах

---
+++ Laba/crypto_interface.h
@@ -0,0 +1,30 @@
+#ifndef CRYPTO_INTERFACE_H
+#define CRYPTO_INTERFACE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct ConstBuffer {
+    const uint8_t* data;
+    size_t size;
+};
+
+struct MutBuffer {
+    uint8_t* data;
+    size_t size;
+};
+
+struct AlgorithmInfo {
+    const char* algorithm_name;
+    size_t key_size;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
